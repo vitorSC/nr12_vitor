a08a31ddc7a37ecd03eac631983091b27fd2c227
Vitor Schweder <vitor.scw@gmail.com>
2013-06-20 01:46:34 -0300

    FormCollection2

--- module/Nr12/src/Nr12/Form/ProdutoFieldset.php
+++ module/Nr12/src/Nr12/Form/ProdutoFieldset.php
@@ -25,28 +25,14 @@ class ProdutoFieldset extends Fieldset implements InputFilterProviderInterface
 		));
 
 		$categoriaFieldset = new CategoriaFieldset($objectManager);
-		/* $this->add(array(
+		$this->add(array(
 			'type'    => 'Zend\Form\Element\Collection',
 			'name'    => 'categorias',
 			'options' => array(
 				'count' => 2,
 				'target_element' => $categoriaFieldset,				
 			)
-		)); */
-		
-		$this->add(
-    		array(
-        		'type' => 'DoctrineModule\Form\Element\ObjectSelect',
-        		'name' => 'name',
-        		'options' => array(
-            		'object_manager'  => $objectManager,
-            		'target_class'    => 'Nr12\Model\Categoria',
-            		'label_generator' => function($targetEntity) {
-                		return $targetEntity->getCatId() . ' - ' . $targetEntity->getDescricao();
-            		},
-        		),
-    		)
-		);
+		)); 				
 		
 	}
 
